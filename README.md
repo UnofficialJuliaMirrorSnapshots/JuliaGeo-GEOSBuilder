aee33364f44293e546becc5dd5936cbd873e4c75
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 22:58:45 +0000

    Snapshot of branch master taken on 2019-04-07T18:58:44.355-04:00 from "https://github.com/UnofficialJuliaMirror/JuliaGeo-GEOSBuilder"

---
+++ README.md
@@ -0,0 +1,9 @@
+# GEOSBuilder
+
+[![Build Status](https://travis-ci.org/JuliaGeo/GEOSBuilder.svg?branch=master)](https://travis-ci.org/JuliaGeo/GEOSBuilder)
+
+This repository builds binary artifacts for the GEOS project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/JuliaGeo/GEOSBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,41 @@
+using BinaryBuilder
+
+src_version = v"3.7.1"  # also change in raw script string below
+
+# Collection of sources required to build GEOS
+sources = [
+    "http://download.osgeo.org/geos/geos-$src_version.tar.bz2" =>
+    "0006c7b49eaed016b9c5c6f872417a7d7dc022e069ddd683335793d905a8228c",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+# use GCC ar instead of the LLVM one on OSX,
+# see https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/388
+if [[ ${target} == *darwin* ]]; then
+    export AR=/opt/${target}/bin/${target}-ar
+fi
+
+cd $WORKSPACE/srcdir
+cd geos-3.7.1/
+./configure --prefix=$prefix --host=$target
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+platforms = expand_gcc_versions(platforms)
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libgeos_c", :libgeos),
+    LibraryProduct(prefix, "libgeos", :libgeos_cpp)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = []
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "GEOS", src_version, sources, script, platforms, products, dependencies)
